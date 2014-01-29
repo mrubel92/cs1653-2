5b4fa0014cb513a62d0b8e7c4adcadc78d1fad09
Tim O <tdoshea90@yahoo.com>
2014-01-29 15:21:10 -0500

    Implemented token class

--- src/Token.java
+++ src/Token.java
@@ -3,26 +3,32 @@ import java.util.List;
 
 public class Token implements UserToken {
 
+	String issuer;
+	String subject;
+	ArrayList<String> usersGroups;
+	
 	public Token(String name, String username, ArrayList<String> userGroups) {
-		// TODO Auto-generated constructor stub
+		issuer = name;
+		subject = username;
+		usersGroups = new ArrayList<String>(userGroups.size());
+		for (String group : userGroups) {
+			usersGroups.add(group);
+		}
 	}
 
 	@Override
 	public String getIssuer() {
-		// TODO Auto-generated method stub
-		return null;
+		return issuer;
 	}
 
 	@Override
 	public String getSubject() {
-		// TODO Auto-generated method stub
-		return null;
+		return subject;
 	}
 
 	@Override
 	public List<String> getGroups() {
-		// TODO Auto-generated method stub
-		return null;
+		return usersGroups;
 	}
 
 }
